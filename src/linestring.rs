155159716ac7a859cc68c9a38efb3d61ab7b852e
thehappycheese <the.nicholas.archer@gmail.com>
2021-04-14 20:30:37 +0800

    big refactor to structs

--- src/linestring.rs
+++ src/linestring.rs
@@ -1,107 +1,158 @@
-use crate::vector2::Vector2;
 use crate::pairable::Pairable;
+use crate::vector2::Vector2;
 
-
-pub type LineStringPlain = Vec<Vector2>;
-pub type LineSegmentMeasured = (Vector2, Vector2, f64);
-pub type LineStringMeasured = (Vec<LineSegmentMeasured>, f64);
-
-
-pub trait LineSegmenty{
-	fn intersect(self, other:LineSegmentMeasured) -> Option<(Vector2, f64, f64)>;
+#[derive(Clone, Debug, PartialEq)]
+pub struct LineString {
+	pub points: Vec<Vector2>,
 }
 
-impl LineSegmenty for LineSegmentMeasured{
-	
-	fn intersect(self, other:LineSegmentMeasured) -> Option<(Vector2, f64, f64)>{
-		let (a, b, _ab_len) = self;
-		let ab =  b - a;
-		let (c, d, _cd_len) = other;
-		let cd =  d - c;
+#[derive(Clone, Debug,PartialEq)]
+pub struct LineSegmentMeasured {
+	a: Vector2,
+	b: Vector2,
+	mag: f64,
+}
+
+#[derive(Clone, Debug,PartialEq)]
+pub struct LineStringMeasured {
+	segments: Vec<LineSegmentMeasured>,
+	mag: f64,
+}
+
+pub trait LineSegmenty {
+	fn intersect(&self, other: &LineSegmentMeasured) -> Option<(Vector2, f64, f64)>;
+}
+
+impl LineSegmenty for LineSegmentMeasured {
+	fn intersect(&self, other: &LineSegmentMeasured) -> Option<(Vector2, f64, f64)> {
+		let LineSegmentMeasured { a, b, mag: _ab_len } = self;
+		let ab = b - a;
+		let LineSegmentMeasured {
+			a: c,
+			b: d,
+			mag: _cd_len,
+		} = other;
+		let cd = d - c;
 
 		let ab_cross_cd = ab.cross(cd);
-		if ab_cross_cd == 0f64{
+		if ab_cross_cd == 0f64 {
 			return None;
 		}
-		let ac = c - a;
-		let time_ab =  ac.cross(cd) / ab_cross_cd;
+		let ac = c - &a;
+		let time_ab = ac.cross(cd) / ab_cross_cd;
 		let time_cd = -ab.cross(ac) / ab_cross_cd;
 
-		Some((a+ab*time_ab, time_ab, time_cd))
+		Some((a + &(ab * time_ab), time_ab, time_cd))
 	}
 }
-
 
 pub trait LineStringy {
 	//fn iter_segments(&self) -> std::iter::Zip<std::slice::Iter<Vector2>, std::slice::Iter<Vector2>>;
 	fn magnitude(&self) -> f64;
 	fn measured_segments(&self) -> LineStringMeasured;
 	fn offset_segments(&self, distance: f64) -> Vec<LineSegmentMeasured>;
-	fn cut_linestring(&self, fraction_of_length: f64) -> (Option<LineStringMeasured>, Option<LineStringMeasured>);
-	fn direction(&self, fraction_of_length:f64) -> f64;
+	fn double_cut_linestring(
+		&self,
+		fraction_of_length_start: f64,
+		fraction_of_length_end: f64,
+	) -> (
+		Option<LineStringMeasured>,
+		Option<LineStringMeasured>,
+		Option<LineStringMeasured>,
+	);
+	fn cut_linestring(
+		&self,
+		fraction_of_length: f64,
+	) -> (Option<LineStringMeasured>, Option<LineStringMeasured>);
+	fn direction(&self, fraction_of_length: f64) -> f64;
 
-	/// Offset algorithim that performs no cleanup. 
+	/// Offset algorithim that performs no cleanup.
 	/// Each segment is offset, then rejoined in the most naieve way.
-	fn basic_offset(&self, distance:f64) -> Option<LineStringPlain>;
+	fn basic_offset(&self, distance: f64) -> Option<LineString>;
 }
 
-
-impl LineStringy for LineStringPlain {
-
+impl LineStringy for LineString {
 	fn magnitude(&self) -> f64 {
-		self.pairwise().map(|(a, b)| (b - a).magnitude()).sum()
+		self.points
+			.pairwise()
+			.map(|(a, b)| (b - a).magnitude())
+			.sum()
 	}
 
 	fn offset_segments<'a>(self: &'a Self, distance: f64) -> Vec<LineSegmentMeasured> {
-		self.pairwise()
+		self.points
+			.pairwise()
 			.map(move |(a, b)| {
 				let ab = b - a;
 				let mag = ab.magnitude();
 				let offset_vector = ab.left() / mag * distance;
-				(a + &offset_vector, b + &offset_vector, mag)
+				LineSegmentMeasured {
+					a: a + &offset_vector,
+					b: b + &offset_vector,
+					mag,
+				}
 			})
 			.collect()
 	}
 
 	fn measured_segments(&self) -> LineStringMeasured {
 		let mut sum_mag = 0f64;
-		let mut vec_part: Vec<LineSegmentMeasured> = Vec::with_capacity(self.len()-1);
-		for (&a, &b) in self.pairwise(){
+		let mut vec_part: Vec<LineSegmentMeasured> = Vec::with_capacity(self.points.len() - 1);
+		for (&a, &b) in self.points.pairwise() {
 			let ab_mag = a.distance_to(b);
-			sum_mag+=ab_mag;
-			vec_part.push((a, b, ab_mag));
+			sum_mag += ab_mag;
+			vec_part.push(LineSegmentMeasured { a, b, mag: ab_mag });
 		}
-		(vec_part, sum_mag)
+		LineStringMeasured {
+			segments: vec_part,
+			mag: sum_mag,
+		}
 	}
-	
-	fn cut_linestring(&self,fraction_of_length: f64) -> (Option<LineStringMeasured>, Option<LineStringMeasured>) {
+
+	fn cut_linestring(
+		&self,
+		fraction_of_length: f64,
+	) -> (Option<LineStringMeasured>, Option<LineStringMeasured>) {
 		let mls = self.clone().measured_segments();
 		mls.cut_linestring(fraction_of_length)
 	}
+	fn double_cut_linestring(
+		&self,
+		fraction_of_length_start: f64,
+		fraction_of_length_end: f64,
+	) -> (
+		Option<LineStringMeasured>,
+		Option<LineStringMeasured>,
+		Option<LineStringMeasured>,
+	) {
+		let mls = self.clone().measured_segments();
+		mls.double_cut_linestring(fraction_of_length_start, fraction_of_length_end)
+	}
 
-	fn direction(&self, fraction_of_length: f64) -> f64{
+	fn direction(&self, fraction_of_length: f64) -> f64 {
 		self.measured_segments().direction(fraction_of_length)
 	}
 
-	fn basic_offset(&self, distance: f64)->Option<LineStringPlain>{
+	fn basic_offset(&self, distance: f64) -> Option<LineString> {
 		self.measured_segments().basic_offset(distance)
 	}
 }
 
-
 impl LineStringy for LineStringMeasured {
-
 	fn magnitude(&self) -> f64 {
-		let (_measured_segments, self_magnitude) = self;
-		return *self_magnitude;
+		return self.mag;
 	}
 
 	fn offset_segments(&self, distance: f64) -> Vec<LineSegmentMeasured> {
-		let (measured_segments, _total_length) = self;
-		measured_segments.iter()
-			.map(move |(a, b, c)| {
+		self.segments
+			.iter()
+			.map(move |LineSegmentMeasured { a, b, mag }| {
 				let offset_vector = (b - a).left().unit() * distance;
-				(a + &offset_vector, b + &offset_vector, *c)
+				LineSegmentMeasured {
+					a: a + &offset_vector,
+					b: b + &offset_vector,
+					mag: *mag,
+				}
 			})
 			.collect()
 	}
@@ -110,43 +161,67 @@ impl LineStringy for LineStringMeasured {
 		self.clone()
 	}
 
-	fn cut_linestring(&self, fraction_of_length: f64) -> (Option<LineStringMeasured>, Option<LineStringMeasured>){
-		let (measured_segments, self_magnitude) = self;
-		
-		let distance_along = self_magnitude * fraction_of_length;
+	fn cut_linestring(
+		&self,
+		fraction_of_length: f64,
+	) -> (Option<LineStringMeasured>, Option<LineStringMeasured>) {
+		//let (segments, mag) = self;
+		let distance_along = self.mag * fraction_of_length;
 
 		if distance_along <= 0f64 {
 			return (None, Some(self.clone()));
-		} else if distance_along >= *self_magnitude {
+		} else if distance_along >= self.mag {
 			return (Some(self.clone()), None);
 		} else {
 			let mut distance_so_far = 0f64;
 			let mut distance_remaining = distance_along;
-			for (index, (a, b, segment_length)) in measured_segments.iter().enumerate() {
+			for (
+				index,
+				LineSegmentMeasured {
+					a,
+					b,
+					mag: segment_length,
+				},
+			) in self.segments.iter().enumerate()
+			{
 				if distance_remaining <= 0f64 {
 					return (
-						Some((measured_segments[..index + 1].to_vec(), distance_along)),
-						Some((measured_segments[index..].to_vec(), self_magnitude-distance_along)),
+						Some(LineStringMeasured {
+							segments: self.segments[..index].to_vec(),
+							mag: distance_along,
+						}),
+						Some(LineStringMeasured {
+							segments: self.segments[index..].to_vec(),
+							mag: self.mag - distance_along,
+						}),
 					);
 				} else if distance_remaining < *segment_length {
 					let ab_unit = (b - a) / *segment_length;
 					let intermediate_point = *a + ab_unit * distance_remaining;
 
-					let mut part_1 = measured_segments[..index].to_vec();
-					part_1.push((*a, intermediate_point, distance_remaining));
+					let mut part_1 = self.segments[..index].to_vec();
+					part_1.push(LineSegmentMeasured {
+						a: *a,
+						b: intermediate_point,
+						mag: distance_remaining,
+					});
 
-					let mut part_2 = vec![(intermediate_point, *b, segment_length - distance_remaining)];
-					part_2.append(&mut measured_segments[index+1..].to_vec());
+					let mut part_2 = vec![LineSegmentMeasured {
+						a: intermediate_point,
+						b: *b,
+						mag: segment_length - distance_remaining,
+					}];
+					part_2.append(&mut self.segments[index + 1..].to_vec());
 
 					return (
-						Some((
-							part_1,
-							distance_so_far+distance_remaining
-						)), 
-						Some((
-							part_2,
-							self_magnitude-distance_so_far-distance_remaining
-						))
+						Some(LineStringMeasured {
+							segments: part_1,
+							mag: distance_so_far + distance_remaining,
+						}),
+						Some(LineStringMeasured {
+							segments: part_2,
+							mag: self.mag - distance_so_far - distance_remaining,
+						}),
 					);
 				}
 				distance_so_far += segment_length;
@@ -156,187 +231,248 @@ impl LineStringy for LineStringMeasured {
 		return (None, None);
 	}
 
-	fn direction(&self, fraction_of_length: f64) -> f64{
-		let (measured_segments, self_magnitude) = self;
-		let de_normalised_distance_along = self_magnitude * fraction_of_length;
+	fn double_cut_linestring(
+		&self,
+		fraction_of_length_start: f64,
+		fraction_of_length_end: f64,
+	) -> (
+		Option<LineStringMeasured>,
+		Option<LineStringMeasured>,
+		Option<LineStringMeasured>,
+	) {
+		let (a, bc) = self.cut_linestring(fraction_of_length_start);
+		match bc {
+			Some(bc) => {
+				let a_fraction_of_length = f64::max(fraction_of_length_start, 0f64);
+				let bc_fraction_of_length = 1f64 - a_fraction_of_length;
+				// if bc_fraction_of_length <= 0f64 {
+				// 	return (a, None, None)
+				// }
+				let (b, c) = bc.cut_linestring(
+					(fraction_of_length_end - a_fraction_of_length) / bc_fraction_of_length,
+				);
+				(a, b, c)
+			}
+			None => (a, None, None),
+		}
+	}
+
+	fn direction(&self, fraction_of_length: f64) -> f64 {
+		let de_normalised_distance_along = self.mag * fraction_of_length;
 		let mut len_so_far = 0f64;
-		for (a, b, segment_length) in measured_segments{
+		for LineSegmentMeasured{a, b, mag:segment_length} in &self.segments {
 			len_so_far += segment_length;
-			if len_so_far >= de_normalised_distance_along{
-				return (b-a).direction();
+			if len_so_far >= de_normalised_distance_along {
+				return (b - a).direction();
 			}
 		}
 		return 0f64;
 	}
 
-
-	fn basic_offset(&self, distance: f64) -> Option<LineStringPlain>{
-		let (self_segments, _self_magnitude) = self;
-		if self_segments.len() == 0{
-			return None
+	fn basic_offset(&self, distance: f64) -> Option<LineString> {
+		
+		if self.segments.len() == 0 {
+			return None;
 		}
-		
+
 		let offset_segments = self.offset_segments(distance);
-		// this is not required since the pairwise loop will not run if there is only 1 element
-		// if measured_segments.len()==1 {
-		// 	let (a, b, _) = offset_segments[0];
-		// 	return Some(vec![a,b])
-		// }
-		
-		
-		let mut result:LineStringPlain = Vec::with_capacity(offset_segments.len() + 5);
-		
-		let (first_point, _, _) = offset_segments[0];
-		result.push(first_point);
 
-		for (mseg1, mseg2) in offset_segments.pairwise(){
-			let (a, b, _) = mseg1;
-			let (c, d, _) = mseg2;
-			let ab = b-a;
-			let cd = d-c;
-			if ab.cross(cd).abs()<0.00000001{
-				result.push(*b);
-			}else if let Some((intersection_point, time_ab, time_cd)) = mseg1.intersect(*mseg2){
+		let mut points = Vec::with_capacity(offset_segments.len() + 5);
+		
+		points.push(offset_segments[0].a);
 
+		for (mseg1, mseg2) in offset_segments.pairwise() {
+			let LineSegmentMeasured{a, b, mag:_} = mseg1;
+			let LineSegmentMeasured{a:c, b:d, mag:_} = mseg2;
+			let ab = b - a;
+			let cd = d - c;
+			if ab.cross(cd).abs() < 0.00000001 {
+				points.push(*b);
+			} else if let Some((intersection_point, time_ab, time_cd)) = mseg1.intersect(mseg2) {
 				let tip_ab = 0f64 <= time_ab && time_ab <= 1f64;
 				let fip_ab = !tip_ab;
 				let pfip_ab = fip_ab && time_ab > 0f64;
-				
 				let tip_cd = 0f64 <= time_cd && time_cd <= 1f64;
 				let fip_cd = !tip_cd;
 
-				if tip_ab && tip_cd{
+				if tip_ab && tip_cd {
 					// Case 2a
 					// TODO: test for mitre limit
-					result.push(intersection_point);
-				}else if fip_ab && fip_cd{
+					points.push(intersection_point);
+				} else if fip_ab && fip_cd {
 					// Case 2b.
-					if pfip_ab{
+					if pfip_ab {
 						// TODO: test for mitre limit
-						result.push(intersection_point);
-					}else{
-						result.push(*b);
-						result.push(*c);
+						points.push(intersection_point);
+					} else {
+						points.push(*b);
+						points.push(*c);
 					}
-				}else{
+				} else {
 					// Case 2c. (either ab or cd
-					result.push(*b);
-					result.push(*c);
+						points.push(*b);
+						points.push(*c);
 				}
 			}
-
 		}
-		let (_, last_point, _) = offset_segments[offset_segments.len()-1];
-		result.push(last_point);
-		Some(result)
+		points.push(offset_segments[offset_segments.len() - 1].b);
+		Some(LineString{points})
 	}
-
 }
-
-
-
-
-
 
 #[cfg(test)]
 mod tests {
-	use crate::linestring::LineStringy;
+	use crate::linestring::{LineStringy, LineString, LineStringMeasured, LineSegmentMeasured};
 	use crate::vector2::Vector2;
 
 	#[test]
 	fn test_linestring_length() {
-		let ls = vec![
+		let ls = LineString{points:vec![
 			Vector2::new(0.0, 0.0),
 			Vector2::new(1.0, 0.0),
 			Vector2::new(1.0, 1.0),
-		];
+		]};
 		assert_eq!(ls.magnitude(), 2f64);
 	}
 
 	#[test]
 	fn test_offset_segments() {
-		let ls = vec![
+		let ls = LineString{points:vec![
 			Vector2::new(0.0, 0.0),
 			Vector2::new(1.0, 0.0),
 			Vector2::new(1.0, 1.0),
-		];
+		]};
 		assert_eq!(
 			ls.offset_segments(1f64),
 			vec![
-				(Vector2::new(0.0, 1.0), Vector2::new(1.0, 1.0), 1.0f64),
-				(Vector2::new(0.0, 0.0), Vector2::new(0.0, 1.0), 1.0f64),
+				LineSegmentMeasured{a:Vector2::new(0.0, 1.0), b:Vector2::new(1.0, 1.0), mag:1.0f64},
+				LineSegmentMeasured{a:Vector2::new(0.0, 0.0), b:Vector2::new(0.0, 1.0), mag:1.0f64},
 			]
 		);
 	}
 	#[test]
 	fn test_measured() {
-		let ls = vec![
+		let ls = LineString{points:vec![
 			Vector2::new(0.0, 0.0),
 			Vector2::new(1.0, 0.0),
 			Vector2::new(1.0, 1.0),
-		];
+		]};
 		//println!("{:?}", ls);
 		let ls_m = ls.measured_segments();
 		//println!("{:?}", ls_m);
 		assert_eq!(
 			ls_m,
-			(
-				vec![
-					(Vector2::new(0.0, 0.0), Vector2::new(1.0, 0.0), 1.0f64),
-					(Vector2::new(1.0, 0.0), Vector2::new(1.0, 1.0), 1.0f64)
+			LineStringMeasured{
+				segments:vec![
+					LineSegmentMeasured{a:Vector2::new(0.0, 0.0), b:Vector2::new(1.0, 0.0), mag:1.0f64},
+					LineSegmentMeasured{a:Vector2::new(1.0, 0.0), b:Vector2::new(1.0, 1.0), mag:1.0f64}
 				],
-				2f64
-			)
+				mag:2f64
+			}
 		);
 	}
 	#[test]
 	fn test_cut() {
-		let ls = vec![
+		let ls = LineString{points:vec![
 			Vector2::new(0.0, 0.0),
 			Vector2::new(1.0, 0.0),
 			Vector2::new(1.0, 1.0),
 			Vector2::new(0.0, 1.0),
-		];
+		]};
 		//println!("{:?}", ls);
 		let ls_c = ls.cut_linestring(0.5f64);
 		//println!("{:?}", ls_m);
 		assert_eq!(
 			ls_c,
 			(
-				Some((
-					vec![
-						(Vector2::new(0.0, 0.0), Vector2::new(1.0, 0.0), 1.0f64),
-						(Vector2::new(1.0, 0.0), Vector2::new(1.0, 0.5), 0.5f64)
+				Some(LineStringMeasured{
+					segments:vec![
+						LineSegmentMeasured{a:Vector2::new(0.0, 0.0), b:Vector2::new(1.0, 0.0), mag:1.0f64},
+						LineSegmentMeasured{a:Vector2::new(1.0, 0.0), b:Vector2::new(1.0, 0.5), mag:0.5f64}
 					],
-					1.5f64
-				)),
-				Some((
-					vec![
-						(Vector2::new(1.0, 0.5), Vector2::new(1.0, 1.0), 0.5f64),
-						(Vector2::new(1.0, 1.0), Vector2::new(0.0, 1.0), 1.0f64)
+					mag:1.5f64
+				}),
+				Some(LineStringMeasured{
+					segments:vec![
+						LineSegmentMeasured{a:Vector2::new(1.0, 0.5), b:Vector2::new(1.0, 1.0), mag:0.5f64},
+						LineSegmentMeasured{a:Vector2::new(1.0, 1.0), b:Vector2::new(0.0, 1.0), mag:1.0f64}
 					],
-					1.5f64
-				))
+					mag:1.5f64
+				})
 			)
 		);
 		let ls_c = ls.cut_linestring(0f64);
 		//println!("{:?}", ls_m);
-		assert_eq!(
-			ls_c,
-			(
-				None,
-				Some(ls.measured_segments())
-			)
-		);
+		assert_eq!(ls_c, (None, Some(ls.measured_segments())));
 
 		let ls_c = ls.cut_linestring(1f64);
 		//println!("{:?}", ls_m);
+		assert_eq!(ls_c, (Some(ls.measured_segments()), None,));
+
+		let ls = LineString{points:vec![
+			Vector2::new(0.0, 0.0),
+			Vector2::new(1.0, 0.0),
+			Vector2::new(1.0, 1.0),
+			Vector2::new(0.0, 1.0),
+			Vector2::new(0.0, 2.0),
+		]};
+		let (a, b) = ls.cut_linestring(0.5f64);
 		assert_eq!(
-			ls_c,
-			(
-				Some(ls.measured_segments()),
-				None,
-			)
+			b,
+			Some(LineStringMeasured{
+				segments:vec![
+					LineSegmentMeasured{a:Vector2::new(1.0, 1.0), b:Vector2::new(0.0, 1.0), mag:1.0f64},
+					LineSegmentMeasured{a:Vector2::new(0.0, 1.0), b:Vector2::new(0.0, 2.0), mag:1.0f64}
+				],
+				mag:2f64
+			})
+		);
+		assert_eq!(
+			a,
+			Some(LineStringMeasured{
+				segments:vec![
+					LineSegmentMeasured{a:Vector2::new(0.0, 0.0), b:Vector2::new(1.0, 0.0), mag:1.0f64},
+					LineSegmentMeasured{a:Vector2::new(1.0, 0.0), b:Vector2::new(1.0, 1.0), mag:1.0f64}
+				],
+				mag:2f64
+			})
+		);
+	}
+
+	#[test]
+	fn test_double_cut() {
+		let ls = LineString{points:vec![
+			Vector2::new(0.0, 0.0),
+			Vector2::new(1.0, 0.0),
+			Vector2::new(1.0, 1.0),
+			Vector2::new(0.0, 1.0),
+			Vector2::new(0.0, 2.0),
+		]};
+		//println!("{:?}", ls);
+		let (a, b, c) = ls.double_cut_linestring(0.5f64, 0.75f64);
+		assert_eq!(
+			a,
+			Some(LineStringMeasured{
+				segments:vec![
+					LineSegmentMeasured{a:Vector2::new(0.0, 0.0), b:Vector2::new(1.0, 0.0), mag:1.0f64},
+					LineSegmentMeasured{a:Vector2::new(1.0, 0.0), b:Vector2::new(1.0, 1.0), mag:1.0f64}
+				],
+				mag:2f64
+			})
+		);
+		assert_eq!(
+			b,
+			Some(LineStringMeasured{
+				segments:vec![LineSegmentMeasured{a:Vector2::new(1.0, 1.0), b:Vector2::new(0.0, 1.0), mag:1.0f64},],
+				mag:1f64
+			})
+		);
+		assert_eq!(
+			c,
+			Some(LineStringMeasured{
+				segments:vec![LineSegmentMeasured{a:Vector2::new(0.0, 1.0), b:Vector2::new(0.0, 2.0), mag:1.0f64},],
+				mag:1f64
+			})
 		);
 	}
 
@@ -352,17 +488,34 @@ mod tests {
 		// let lsbo = ls.basic_offset(5f64);
 		// println!("{:?}",lsbo);
 
-		let ls = vec![
+		let ls = LineString{points:vec![
 			Vector2::new(0.0, 0.0),
 			Vector2::new(1.0, 1.0),
 			Vector2::new(1.5, 2.0),
 			Vector2::new(1.0, 3.0),
-		];
+		]};
 		let lsbo = ls.basic_offset(-0.5f64);
-		println!("{:?}",lsbo);
+		println!("{:?}", lsbo);
 		assert_eq!(
 			lsbo,
-			Some(vec![Vector2 { x: 0.35355339059327373, y: -0.35355339059327373 }, Vector2 { x: 1.4109272075633472, y: 0.7038204263767998 }, Vector2 { x: 2.0590169943749475, y: 2.0 }, Vector2 { x: 1.4472135954999579, y: 3.223606797749979 }])
+			Some(LineString{points:vec![
+				Vector2 {
+					x: 0.35355339059327373,
+					y: -0.35355339059327373
+				},
+				Vector2 {
+					x: 1.4109272075633472,
+					y: 0.7038204263767998
+				},
+				Vector2 {
+					x: 2.0590169943749475,
+					y: 2.0
+				},
+				Vector2 {
+					x: 1.4472135954999579,
+					y: 3.223606797749979
+				}
+			]})
 		);
 	}
 }
--- src/vector2.rs
+++ src/vector2.rs
@@ -1,9 +1,18 @@
 use std::ops;
+use serde::{Deserialize, Serialize};
+use serde::ser::Serializer;
 
-#[derive(Debug, PartialEq, Clone, Copy)]
+#[derive(Debug, PartialEq, Clone, Copy, Deserialize)]
 pub struct Vector2 {
 	pub x: f64,
 	pub y: f64,
+}
+
+impl Serialize for Vector2 {
+	fn serialize<S>(&self, serialiser:S) -> Result<S::Ok, S::Error>
+	where S:Serializer {
+		(&self.x, &self.y).serialize(serialiser)
+	}
 }
 
 impl Vector2 {
@@ -126,110 +135,110 @@ impl ops::Mul<f64> for Vector2 {
 
 #[cfg(test)]
 mod tests {
-    use crate::vector2::{Vector2};
+	use crate::vector2::{Vector2};
 
-    #[test]
-    fn create_vector() {
-        let v = Vector2::new(1.0, 2.0);
-        assert_eq!(v.x, 1.0);
-        assert_eq!(v.y, 2.0);
-    }
+	#[test]
+	fn create_vector() {
+		let v = Vector2::new(1.0, 2.0);
+		assert_eq!(v.x, 1.0);
+		assert_eq!(v.y, 2.0);
+	}
 
-    #[test]
-    fn equality_derived() {
-        let v1 = Vector2::new(1.0, 2.0);
-        let v2 = Vector2::new(1.0, 2.0);
-        let v3 = Vector2::new(1.0001, 2.0);
-        assert_eq!(v1, v2);
-        assert_ne!(v1, v3);
-    }
-    #[test]
-    fn clone_derived() {
-        let v1 = Vector2::new(1.0, 2.0);
-        let v2 = v1.clone();
-        assert_eq!(v1, v2);
-        
-    }
-    #[test]
-    fn magnitude_squared() {
-        let v1 = Vector2::new(3.0, 4.0);
-        assert_eq!(v1.magnitude_squared(), 25.0);
-    }
-    #[test]
-    fn magnitude() {
-        let v1 = Vector2::new(3.0, 4.0);
-        assert_eq!(v1.magnitude(), 5.0);
-    }
-    #[test]
-    fn sub() {
-        let v1 = Vector2::new(3.0, 4.0);
-        let v2 = Vector2::new(1.0, 1.0);
-        let v3 = v1 - v2;
-        assert_eq!(v3.x, 2.0);
-        assert_eq!(v3.y, 3.0);
-    }
-    #[test]
-    fn add() {
-        let v1 = Vector2::new(3.0, 4.0);
-        let v2 = Vector2::new(1.0, 1.0);
-        let v3 = v1 + v2;
-        assert_eq!(v3.x, 4.0);
-        assert_eq!(v3.y, 5.0);
-    }
-    #[test]
-    fn neg() {
-        let v1 = Vector2::new(3.0, 4.0);
-        let v2 = -v1;
-        assert_eq!(v2.x, -3.0);
-        assert_eq!(v2.y, -4.0);
-    }
-    #[test]
-    fn mul() {
-        let v1 = Vector2::new(3.0, 4.0);
-        let v3 = v1*2.0;
-        assert_eq!(v3.x, 6.0);
-        assert_eq!(v3.y, 8.0);
-    }
-    #[test]
-    fn div() {
-        let v1 = Vector2::new(3.0, 4.0);
-        let v3 = v1/2.0;
-        assert_eq!(v3.x, 1.5);
-        assert_eq!(v3.y, 2.0);
-    }
-    #[test]
-    fn dot() {
-        let v1 = Vector2::new(3.0, 4.0);
-        let v2 = Vector2::new(5.0, 6.0);
-        assert_eq!(v1.dot(v2), 3.0*5.0 + 4.0*6.0);
-    }
-    #[test]
-    fn cross() {
-        let v1 = Vector2::new(3.0, 4.0);
-        let v2 = Vector2::new(5.0, 6.0);
-        assert_eq!(v1.cross(v2), 3.0*6.0 - 4.0*5.0);
-    }
-    #[test]
-    fn left() {
-        let v1 = Vector2::new(3.0, 4.0);
-        let v2 = v1.left();
-        assert_eq!(v2.x, -4.0);
-        assert_eq!(v2.y, 3.0);
-    }
-    #[test]
-    fn right() {
-        let v1 = Vector2::new(3.0, 4.0);
-        let v2 = v1.right();
-        assert_eq!(v2.x, 4.0);
-        assert_eq!(v2.y, -3.0);
-    }
-    #[test]
-    fn unit() {
-        let v1 = Vector2::new(3.0, 4.0);
-        let v2 = v1.unit();
-        let v3 = v2 * v1.magnitude();
-        assert_eq!(v2.magnitude(), 1.0);
-        assert_eq!(v3.x, 3.0);
-        assert_eq!(v3.y, 4.0);
-    }
+	#[test]
+	fn equality_derived() {
+		let v1 = Vector2::new(1.0, 2.0);
+		let v2 = Vector2::new(1.0, 2.0);
+		let v3 = Vector2::new(1.0001, 2.0);
+		assert_eq!(v1, v2);
+		assert_ne!(v1, v3);
+	}
+	#[test]
+	fn clone_derived() {
+		let v1 = Vector2::new(1.0, 2.0);
+		let v2 = v1.clone();
+		assert_eq!(v1, v2);
+		
+	}
+	#[test]
+	fn magnitude_squared() {
+		let v1 = Vector2::new(3.0, 4.0);
+		assert_eq!(v1.magnitude_squared(), 25.0);
+	}
+	#[test]
+	fn magnitude() {
+		let v1 = Vector2::new(3.0, 4.0);
+		assert_eq!(v1.magnitude(), 5.0);
+	}
+	#[test]
+	fn sub() {
+		let v1 = Vector2::new(3.0, 4.0);
+		let v2 = Vector2::new(1.0, 1.0);
+		let v3 = v1 - v2;
+		assert_eq!(v3.x, 2.0);
+		assert_eq!(v3.y, 3.0);
+	}
+	#[test]
+	fn add() {
+		let v1 = Vector2::new(3.0, 4.0);
+		let v2 = Vector2::new(1.0, 1.0);
+		let v3 = v1 + v2;
+		assert_eq!(v3.x, 4.0);
+		assert_eq!(v3.y, 5.0);
+	}
+	#[test]
+	fn neg() {
+		let v1 = Vector2::new(3.0, 4.0);
+		let v2 = -v1;
+		assert_eq!(v2.x, -3.0);
+		assert_eq!(v2.y, -4.0);
+	}
+	#[test]
+	fn mul() {
+		let v1 = Vector2::new(3.0, 4.0);
+		let v3 = v1*2.0;
+		assert_eq!(v3.x, 6.0);
+		assert_eq!(v3.y, 8.0);
+	}
+	#[test]
+	fn div() {
+		let v1 = Vector2::new(3.0, 4.0);
+		let v3 = v1/2.0;
+		assert_eq!(v3.x, 1.5);
+		assert_eq!(v3.y, 2.0);
+	}
+	#[test]
+	fn dot() {
+		let v1 = Vector2::new(3.0, 4.0);
+		let v2 = Vector2::new(5.0, 6.0);
+		assert_eq!(v1.dot(v2), 3.0*5.0 + 4.0*6.0);
+	}
+	#[test]
+	fn cross() {
+		let v1 = Vector2::new(3.0, 4.0);
+		let v2 = Vector2::new(5.0, 6.0);
+		assert_eq!(v1.cross(v2), 3.0*6.0 - 4.0*5.0);
+	}
+	#[test]
+	fn left() {
+		let v1 = Vector2::new(3.0, 4.0);
+		let v2 = v1.left();
+		assert_eq!(v2.x, -4.0);
+		assert_eq!(v2.y, 3.0);
+	}
+	#[test]
+	fn right() {
+		let v1 = Vector2::new(3.0, 4.0);
+		let v2 = v1.right();
+		assert_eq!(v2.x, 4.0);
+		assert_eq!(v2.y, -3.0);
+	}
+	#[test]
+	fn unit() {
+		let v1 = Vector2::new(3.0, 4.0);
+		let v2 = v1.unit();
+		let v3 = v2 * v1.magnitude();
+		assert_eq!(v2.magnitude(), 1.0);
+		assert_eq!(v3.x, 3.0);
+		assert_eq!(v3.y, 4.0);
+	}
 }
